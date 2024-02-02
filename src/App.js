e803c807bf52bc4bc55e097e594e4ada8b246895
devalmazbek <devalmazbek@gmail.com>
2024-02-02 15:45:27 +0600

    bank account

--- src/App.js
+++ src/App.js
@@ -1,23 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+import "./App.css";
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  isActive: true,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "open":
+      return {
+        ...state,
+        balance: state.balance + 500,
+        isActive: false,
+      };
+    case "deposit":
+      return {
+        ...state,
+        balance: state.balance + 150,
+      };
+    case "withdraw":
+      return {
+        ...state,
+        balance: state.balance > 0 ? state.balance - 50 : state.balance,
+      };
+    case "loan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload,
+        balance: state.balance + action.payload,
+      };
+    case "payloan":
+      return {
+        ...state,
+        loan: 0,
+        balance: state.balance - state.loan,
+      };
+    case "close": {
+      if (state.balance === 0 && state.loan === 0) {
+        return {
+          ...initialState,
+        };
+      }
+      return {
+        ...state,
+      };
+    }
+    default:
+      throw new Error("Unkown");
+  }
+}
 
 function App() {
+  const [{ balance, loan, isActive }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>useReducer Bank Account</h1>
+      <p>Balance: {balance}</p>
+      <p>Loan: {loan}</p>
+      <div className="btn">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button
+            disabled={!isActive}
+            onClick={() => dispatch({ type: "open" })}
+          >
+            Open account
+          </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <button
+            disabled={isActive}
+            onClick={() => dispatch({ type: "deposit" })}
+          >
+            Deposit 150
+          </button>
+        </p>
+        <p>
+          <button
+            disabled={isActive}
+            onClick={() => dispatch({ type: "withdraw" })}
+          >
+            Widthdraw 50
+          </button>
+        </p>
+        <p>
+          <button
+            disabled={isActive}
+            onClick={() => dispatch({ type: "loan", payload: 5000 })}
+          >
+            Request a loan of 5000
+          </button>
+        </p>
+        <p>
+          <button
+            disabled={isActive}
+            onClick={() => dispatch({ type: "payloan" })}
+          >
+            pay loan
+          </button>
+        </p>
+        <p>
+          <button
+            disabled={isActive}
+            onClick={() => dispatch({ type: "close" })}
+          >
+            close account
+          </button>
+        </p>
+      </div>
     </div>
   );
 }
